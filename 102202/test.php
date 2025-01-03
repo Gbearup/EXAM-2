572ca4493c6db42605089929570c541134c3066b
Gbearup <bearapo945@gmail.com>
2025-01-03 16:30:23 +0800

    第二題完成

--- 102202/test.php
+++ 102202/test.php
@@ -2,14 +2,36 @@
 session_start();
 
 class DB{
-    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db13";
+    protected $dsn="mysql:host=localhost; charset=utf8; dbname=db13";
     protected $pdo;
     protected $table;
 
     function __construct($table){
         $this->table=$table;
-        $this->pdo=new PDO($this->dsn,'root','');
+        $this->pdo=new pdo($this->dsn,'root','');
     }
+
+
+
+function all(...$arg){
+    $sql="SELECT * FROM $this->table";
+    if(!empty($arg[0])){
+       if(is_array($arg[0])){
+        $where=$this->a2s($arg[0]);
+        $sql=$sql . "WHERE" . join("&&", $where);
+       }else{
+        $sql.=$arg[0];
+       }
+       }
+
+    if(!empty($arg[1])){
+        $sql=$sql.$arg[1];
+    }
+
+    return $this->fetchAll($sql);
 }
 
+
+
+}
 ?>
--- front/po.php
+++ front/po.php
@@ -1,1 +1,60 @@
-分類網誌
+<div>
+    目前位置:首頁 > 分類網誌 > <span id='type'>健康新知</span>
+</div>
+
+<style>
+.type,.list-item{
+    display:block;
+    margin:10px 0;
+}
+
+</style>
+
+<fieldset style='width: 150px;display:inline-block;vertical-align:top'>
+<legend>分類網誌</legend>
+        <a href="#" class='type'data-type='1'>健康新知</a>  <!-- 行內標籤 a span-->
+        <a href="#" class='type'data-type='2'>菸害防制</a>
+        <a href="#" class='type'data-type='3'>癌症防治</a>
+        <a href="#" class='type'data-type='4'>慢性病防治</a>
+    
+</fieldset>
+
+<fieldset style='width:500px;display:inline-block';>
+    <legend>文章列表</legend>
+    <div id="postList"></div>
+</fieldset>
+
+
+<script>
+
+getList(1)
+
+$(".type").on('click',function(){
+    // console.log($(this).text())
+   $("#type").text($(this).text())
+
+   let type=$(this).data('type')           //如果有參數是post 沒有就是用get
+   getList(type)   
+})
+
+function getList(type){
+    $("#postList").load("./api/get_list.php",{type}) //我把東西給你去執行 執行完畢之後把資料放在框框裡 ajax的寫法
+}
+
+
+function getPost(id){
+    $("#postList").load("./api/get_post.php",{id})
+}
+
+
+</script>
+
+
+  <!-- 不一樣的寫法
+
+ $(".type").on('click',(e)=>{
+     console.log($(e.target).text())
+    $("#type").text($(e.target).text())
+
+ }) -->
+
